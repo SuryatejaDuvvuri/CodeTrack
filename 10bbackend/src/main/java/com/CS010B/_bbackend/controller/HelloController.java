d1a38e152cd57193a7a468a700d68f32f3f82c89
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2025-06-24 16:09:15 +0530

    Add files

---
+++ 10bbackend/src/main/java/com/CS010B/_bbackend/controller/HelloController.java
@@ -0,0 +1,9 @@
+package main.java.com.CS010B._bbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController 
+{
+    
+}
---
+++ 10bbackend/src/test/java/com/CS010B/_bbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS010B._bbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
